36d25e3050df5d2ba80af03c9a56e939e54b1ef1
Thomas Russell <t.p.russell19@gmail.com>
2021-03-26 11:58:52 -0700

    reduce possible responses to questions and add ABC classes to answers

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// Business Logic
+// count all the As, Bs, and Cs selected
+// determine which group has the most (or tie)
+// display the proper response(s) for the coding language(s) chosen
+
+if ()
+
+// User Interface Logic
+
+$(document).ready() {
+
+}
