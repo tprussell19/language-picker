e6a08924c2bd3c5c123b8516992e58cae735a6a0
Thomas Russell <t.p.russell19@gmail.com>
2021-03-26 22:54:22 -0700

    get jQuery logic to finally work correctly

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,7 @@
 // determine which group has the most (or tie)
 // display the proper response(s) for the coding language(s) chosen
 
-// business logic
 
-
-
-// user interface logic
 
 $(document).ready(function() {
   $("form").submit(function(event) {
@@ -18,5 +14,64 @@ $(document).ready(function() {
     const answer4 = $("input:radio[name=answer4]:checked").val();
     const answer5 = $("input:radio[name=answer5]:checked").val();
     console.log(answer1, answer2, answer3, answer4, answer5);
+
+    let pyhtonAnswer = 0;
+    let cSharpAnswer = 0;
+    let rubyAnswer = 0;
+    
+    if (answer1 === "option1") {
+      pyhtonAnswer = pyhtonAnswer + 1;
+    } else if (answer1 === "option2") {
+      cSharpAnswer = cSharpAnswer + 1;
+    } else if (answer1 === "option3") {
+      rubyAnswer = rubyAnswer + 1;
+    }
+    if (answer2 === "option1") {
+      pyhtonAnswer = pyhtonAnswer + 1;
+    } else if (answer2 === "option2") {
+      cSharpAnswer = cSharpAnswer + 1;
+    } else if (answer2 === "option3") {
+      rubyAnswer = rubyAnswer + 1;
+    }
+    if (answer3 === "option1") {
+      pyhtonAnswer = pyhtonAnswer + 1;
+    } else if (answer3 === "option2") {
+      cSharpAnswer = cSharpAnswer + 1;
+    } else if (answer3 === "option3") {
+      rubyAnswer = rubyAnswer + 1;
+    }
+    if (answer4 === "option1") {
+      pyhtonAnswer = pyhtonAnswer + 1;
+    } else if (answer4 === "option2") {
+      cSharpAnswer = cSharpAnswer + 1;
+    } else if (answer4 === "option3") {
+      rubyAnswer = rubyAnswer + 1;
+    }
+    if (answer5 === "option1") {
+      pyhtonAnswer = pyhtonAnswer + 1;
+    } else if (answer5 === "option2") {
+      cSharpAnswer = cSharpAnswer + 1;
+    } else if (answer5 === "option3") {
+      rubyAnswer = rubyAnswer + 1;
+    }
+    console.log(pyhtonAnswer, cSharpAnswer, rubyAnswer);
+
+    $("#result").show();
+    $("#language-description").show();
+    if (pyhtonAnswer >= 2) {
+      $("#python").show();
+    } else {
+      $("#python").hide();
+    }
+    if (cSharpAnswer >= 2) {
+      $("#cSharp").show();
+    } else {
+      $("#cSharp").hide();
+    }
+    if (rubyAnswer >= 2) {
+      $("#ruby").show();
+    } else {
+      $("#ruby").hide();
+    }
   });
 });
